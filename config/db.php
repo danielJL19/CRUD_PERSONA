2aa4b6d5a096894cd776c70a33c1323769ab85e7
Daniel <danieljmnz15@hotmail.com>
2023-03-08 12:43:40 -0300

    resolucion

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php 
 class DB{
     public static function connect(){
-        $mysqli= new mysqli("localhost","root","","personasdb");
+        $mysqli= new mysqli("localhost","root","Lozano001@","personasdb");
         if (!$mysqli) {
             die("Conexion fallida: ". mysqli_connect_error());
         }
