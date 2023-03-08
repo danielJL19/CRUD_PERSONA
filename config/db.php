46ca236a186a79350bd8face2fe8cb557d08263d
Daniel <danieljmnz15@hotmail.com>
2023-03-08 11:44:54 -0300

    bbdd

--- config/db.php
+++ config/db.php
@@ -2,6 +2,9 @@
 class DB{
     public static function connect(){
         $mysqli= new mysqli("localhost","root","Lozano001@","personasdb");
+        if (!$mysqli) {
+            die("Conexion fallida: ". mysqli_connect_error());
+        }
         return $mysqli;
     }
 }
